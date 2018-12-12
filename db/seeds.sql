8a1216bd8efe433662323d226257bf3912059d83
Brett O'Connell <apocreativeworks@gmail.com>
2018-12-12 16:34:30 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (quote, author) VALUES ('Standard Burger', true);
+
+INSERT INTO burgers (quote, author) VALUES ('Deluxe Burger', false);
+
+INSERT INTO burgers (quote, author) VALUES ('Mega Burger', false);
