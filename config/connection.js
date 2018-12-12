7319b58b0f0b09527b8cae1c993e267ee998ff14
Brett O'Connell <apocreativeworks@gmail.com>
2018-12-12 17:39:45 -0600

    oh god

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "cs_class",
   password: "testPassword",
-  database: "burger_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@ var orm = require("../config/orm.js");
 
 
 var burger = {
-    all: function(cb) {
-        orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
     // Variables for columns and values are arrays
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
-    update: function(objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     }
